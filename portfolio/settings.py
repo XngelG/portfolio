b3134be8cf9a5b82ec944384d8f6dd392fa4d7f9
José Ángel Gallardo Tabares <angelgallardo@tec.mx>
2022-01-24 22:22:31 -0600

    added heroku host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -66,6 +66,7 @@ ROOT_URLCONF = 'portfolio.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://portfoliofrontendxngelgt.herokuapp.com/",
 ]
 
 TEMPLATES = [
