8d8c067e929bca49a1597a1969d1797ffd1ef5b0
José Ángel Gallardo Tabares <angelgallardo@tec.mx>
2022-01-24 22:29:23 -0600

    Heroku fix 3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfoliofrontendxngelgt.herokuapp.com']
 
 
 # Application definition
@@ -64,9 +64,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'portfolio.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://portfoliofrontendxngelgt.herokuapp.com/",
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://portfoliofrontendxngelgt.herokuapp.com',
 ]
 
 TEMPLATES = [
